1997bb55de34417d442861d4d8519fc34fa28e1c
ha-rold1999 <htarimanc@gmail.com>
2023-02-15 14:21:40 +0800

    Added a Loading Screen

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import LaodingScreen from "./Component/LoadingComponent";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LaodingScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
