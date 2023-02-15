169bbc4dd53c178100b605cdadf0c4a2226b7899
ha-rold1999 <htarimanc@gmail.com>
2023-02-15 16:34:00 +0800

    Added Singup Form

--- Component/SingupComponent.js
+++ Component/SingupComponent.js
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Pressable } from "react-native";
+import Styles from "../Style/Component/StyleComponent";
+import FormStyle from "../Style/Component/StyleSignupComponent";
 
 export default function SingupScreen() {
   return (
-    <View>
-      <Text>Hell</Text>
+    <View style={Styles.container}>
+      <View style={FormStyle.form}>
+        <Text style={{ fontSize: 20 }}>Create Your Account</Text>
+        <View style={{ paddingTop: "20%" }}>
+          <Text style={FormStyle.label}>Firstname</Text>
+          <TextInput style={FormStyle.input} />
+          <Text style={FormStyle.label}>Lastname</Text>
+          <TextInput style={FormStyle.input} />
+          <Text style={FormStyle.label}>Contact</Text>
+          <TextInput keyboardType="number-pad" style={FormStyle.input} />
+          <Text style={FormStyle.label}>Birthdate</Text>
+          <TextInput style={FormStyle.input} />
+          <Text style={FormStyle.label}>Address</Text>
+          <TextInput style={FormStyle.input} />
+          <Pressable style={FormStyle.button}>
+            <Text style={FormStyle.buttonText}>Next</Text>
+          </Pressable>
+          <Text style={FormStyle.sideNote}>
+            Already have an account? Login here
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
